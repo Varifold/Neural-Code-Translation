804641fe0cffa4cccc0043b3bd17f69333aee6c2
Ville Karlsson <120398546+Varifold@users.noreply.github.com>
2023-05-31 11:50:53 +0300

    Update main.py

--- neural_circuits/main.py
+++ neural_circuits/main.py
@@ -1,10 +1,37 @@
+"""
+This script runs tests for all circuits. Check the neural_circuits module for details.
+Setting Max_Bits too high can cause issues due to constant tau_m or very large size of the circuit.
+"""
+
 import ttfs2binary, rate2binary, binary2rate, binary2ttfs
 
-if __name__ == "__main__":
+def run_tests(max_bits):
+    """
+    Runs test scripts for various circuits with the provided max_bits value.
+    
+    Args:
+        max_bits (int): The maximum number of bits for the test scripts. 
+    """
+    try:
+        ttfs2binary.test_script(max_bits)
+    except Exception as e:
+        print(f"Error in ttfs2binary test: {e}")
+        
+    try:
+        rate2binary.test_script(max_bits)
+    except Exception as e:
+        print(f"Error in rate2binary test: {e}")
+    
+    try:
+        binary2rate.test_script(max_bits)
+    except Exception as e:
+        print(f"Error in binary2rate test: {e}")
+    
+    try:
+        binary2ttfs.test_script(max_bits)
+    except Exception as e:
+        print(f"Error in binary2ttfs test: {e}")
 
-    #Runs all tests for all circuits. Check neural_circuits for details
-    #Setting Max_Bits too high can cause issues due to constant tau_m or very large size of the circuit
-    ttfs2binary.test_script(max_bits=5)
-    rate2binary.test_script(max_bits=5)
-    binary2rate.test_script(max_bits=5)
-    binary2ttfs.test_script(max_bits=5)
+if __name__ == "__main__":
+    MAX_BITS = 5  # Set your max_bits value here
+    run_tests(MAX_BITS)
