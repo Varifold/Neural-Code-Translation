18ecbbcdc5b25fee08b39cc94d85d2d80fb38f0b
Joni Kamarainen <joni.kamarainen@tuni.fi>
2024-11-22 21:28:51 +0200

    corrected timings

--- timing_diagrams/rate_to_binary_2bit.py
+++ timing_diagrams/rate_to_binary_2bit.py
@@ -2,15 +2,15 @@ import wavedrom
 svg = wavedrom.render("""
 { "signal": [
  { "name": "CLK",             "wave": "P.......", "period": 2  },
- { "name": "R",               "wave": "l.hl..hl........" },
- { "name": "A",               "wave": "l.........hl..hl","node":'a..........b..c.' },
+ { "name": "R",               "wave": "l.hlhlhl........" },
+ { "name": "A",               "wave": "hl..............","node":'a.b.......c...d.' },
  { "name": "V2-left (0x01)",  "wave": "l.....hl........"},
  { "name": "V2-right (0x10)", "wave": "l..............."},
- { "name": "P",               "wave": "l.........hl...." },
- { "name": "T0-left",         "wave": "l..............." },
- { "name": "T0-right",        "wave": "l.........hl...." },
+ { "name": "P",               "wave": "hl...............", "node":'e...........f' },
+ { "name": "T0-left",         "wave": "l...........hl.." },
+ { "name": "T0-right",        "wave": "l...........hl.." },
 ],
- "edge":['a-~>b D5', 'a-~>c D7']
+ "edge":['a-~>b D1', 'b-~>c D5', 'c-~>d D7', 'e-~>f D5']
 
 }""")
 svg.saveas("rate_to_binary_2bit.svg")
--- timing_diagrams/rate_to_binary_subcircuit.py
+++ timing_diagrams/rate_to_binary_subcircuit.py
@@ -1,14 +1,14 @@
 import wavedrom
 svg = wavedrom.render("""
 { "signal": [
- { "name": "CLK",         "wave": "P.......", "period": 2  },
- { "name": "R",           "wave": "l.hlhlhl..hlhl.." },
- { "name": "A",           "wave": "l...hl....hl....", "data":['1']},
- { "name": "0x001",       "wave": "2.2.2.2...2.2...", "data":['0','1','0','1','0','1']},
- { "name": "B",           "wave": "l.........hl...."},
- { "name": "0x010",       "wave": "2...2.....2.....", "data":['0','1','0']},
- { "name": "C",           "wave": "l..............." },
- { "name": "0x100",       "wave": "2.........2.....", "data":['0','1']},
+ { "name": "CLK",         "wave": "P........", "period": 2  },
+ { "name": "R",           "wave": "l.hlhlhl..hlhl...." },
+ { "name": "A",           "wave": "l.....hl....hl....", "data":['1']},
+ { "name": "0x001",       "wave": "2...2.2.2...2.2...", "data":['0','1','0','1','0','1']},
+ { "name": "B",           "wave": "l.............hl.."},
+ { "name": "0x010",       "wave": "2.......2.....2...", "data":['0','1','0']},
+ { "name": "C",           "wave": "l................." },
+ { "name": "0x100",       "wave": "2...............2.", "data":['0','1']},
 ],
 }""")
 svg.saveas("rate_to_binary_subcircuit.svg")
